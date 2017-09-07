ac04dff5b7450a372b1704feb9615223c161c9f5
Damien Carver <carverdamien@gmail.com>
2017-09-07 18:24:18 +0200

    more parsing

--- images/memtier/memtier.py
+++ images/memtier/memtier.py
@@ -8,14 +8,30 @@ def gen_calls(call):
         yield call + ["--client-stats", str(id)]
 
 def work(call):
-    id = call[-1] + "-"
-    n = len(id)
+    loop_id = call[-1] + "-"
+    len_loop_id = len(loop_id)
     for root, dirs, files in os.walk('.'):
         for name in files:
-            if name[:n] == id:
+            if name[:len_loop_id] == loop_id:
                 path = os.path.join(root, name)
+                loop_id, run_id, cg_id, prefix, client_id = os.path.splitext(name)[0].split('-')
                 # TODO: parse content of path and push to influxdb
-                print(path)
+                print(loop_id, run_id, cg_id, prefix, client_id)
+                with open(path) as f:
+                    contents = f.read()
+                    for content in contents.split('\n\n'):
+                        lines = content.split('\n')
+                        title = lines[0]
+                        header = [e for e in lines[1].split(',')]
+                        n = len(header)
+                        for line in lines[2:]:
+                            line = [e for e in line.split(',')]
+                            if len(line) != n: continue
+                            fields = {
+                                header[i] : line[i]
+                                for i in range(n)
+                            }
+                            print(fields)
                 os.remove(path)
 
 spawn_worker = work # debug
