09202b267d8aca0cb05a0085affd96b8bac9e05e
Damien Carver <carverdamien@gmail.com>
2017-09-27 09:11:03 +0200

    unixsort

---
+++ images/unixsort/entrypoint.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#set -x -e
+
+N=$1
+shift
+: ${N:=1000000}
+
+./64/gensort -a ${N} /tmp/unsorted
+time sort -S 1G < /tmp/unsorted > /tmp/sorted
+./64/valsort /tmp/sorted
+
+rm -f /tmp/unsorted /tmp/sorted
