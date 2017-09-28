eb27d8f7543052d1d9a340e5aedc17391b26c978
Damien Carver <carverdamien@gmail.com>
2017-09-28 12:16:58 +0200

    more control

--- projects/acdc/make.sh
+++ projects/acdc/make.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 set -x -e
 
-docker-compose -f metric.yml  up --build -d # Remote machine
-docker-compose -f prepare.yml up --build
-docker-compose -f run.yml     up --build
-docker-compose -f report.yml  up --build    # Remote machine
+: ${STSIZE:=100000000}
+: ${DBSIZE:=10000000}
+
+docker-compose down
+docker-compose -f unrestricted.yml up -d --build
+# Prepare Starts
+docker-compose exec sort     prepare -a ${STSIZE}
+docker-compose exec sysbench prepare --dbsize ${DBSIZE}
+# Prepare Endso
+docker-compose down
+
+docker-compose up -d
+# docker-compose stop graphana # if you dont want online monitoring
+
+# RUN Starts
+# Mysql: |-----|ooooo|-----|ooooo|-----
+# Sort : |-----|-----|-----------------
+PERIODE=$((5*60))
+docker-compose exec -d sort run -S 1G
+until docker-compose exec sort terminated
+do
+    docker-compose exec -d sysbench run --dbsize ${DBSIZE} --duration ${PERIODE}
+    sleep ${PERIODE}
+done
+# RUN Ends
+docker-compose stop collector
+docker-compose start graphana
--- projects/acdc/sysbench/benchmark.py
+++ projects/acdc/sysbench/benchmark.py
@@ -10,8 +10,7 @@ main_parser = argparse.ArgumentParser()
 user = 'root'
 host = 'mysql'
 dbname = 'dbname'
-dbsize = 1000
-duration = 5*60
+#duration = 5*60
 main_subparsers = main_parser.add_subparsers()
 
 sysbench_bin_path = './sysbench/sysbench'
@@ -21,13 +20,14 @@ mysql_call = ['mysql',
               '--host', host,
               '-u', user]
 
-sysbench_call = [sysbench_bin_path,
-                 '--test=%s' % sysbench_lua_path,
-                 '--oltp-table-size=%d' % dbsize,
-                 '--mysql-db=%s' % dbname,
-                 '--mysql-host=%s' % host,
-                 '--mysql-user=root',
-                 '--mysql-password=']
+def sysbench_call(dbsize):
+    return [sysbench_bin_path,
+            '--test=%s' % sysbench_lua_path,
+            '--oltp-table-size=%d' % dbsize,
+            '--mysql-db=%s' % dbname,
+            '--mysql-host=%s' % host,
+            '--mysql-user=root',
+            '--mysql-password=']
 
 sysbench_expected_v05_intermediate_output = """[{}] timestamp: {timestamp}, threads: {threads}, tps: {trps}, reads: {rdps}, writes: {wrps}, response time: {rtps}ms ({}%), errors: {errps}, reconnects:  {recops}"""
 sysbenchoutput_parser = parse.compile(sysbench_expected_v05_intermediate_output)
@@ -45,13 +45,13 @@ def wait_for_server_to_start():
 def prepare(args):
     try:
         subprocess.check_call(mysql_call + ['-e', 'CREATE DATABASE %s' % dbname])
-        subprocess.check_call(sysbench_call + ['prepare'])
+        subprocess.check_call(sysbench_call(args.dbsize) + ['prepare'])
     except Exception as e:
         print(e)
     finally:
         out = subprocess.check_output(mysql_call + ['-D', dbname, '-e', 'select count(*) from sbtest1'])
         count = int(out.split('\n')[1])
-        if count != dbsize:
+        if count != args.dbsize:
             raise Exception('count != dbsize')
         subprocess.check_call(mysql_call + ['-e', 'shutdown'])
 
@@ -66,10 +66,10 @@ def run(args):
         client.write_points([p for p in influx(res)])
     args.callback = callback
 
-    call = sysbench_call + ['--report-interval=1',
+    call = sysbench_call(args.dbsize) + ['--report-interval=1',
                             '--tx-rate=%d' % 0,
                             '--max-requests=0',
-                            '--max-time=%d' % duration,
+                            '--max-time=%d' % args.duration,
                             '--num-threads=%d' % 8,
                             '--oltp-read-only=on',
                             'run']
@@ -97,8 +97,11 @@ def influx(fields, tags={}):
 
 def main():
     prepare_parser = main_subparsers.add_parser('prepare')
+    prepare_parser.add_argument('--dbsize',   dest="dbsize", type=int, nargs='?', default=10000)
     prepare_parser.set_defaults(func=prepare)
     run_parser = main_subparsers.add_parser('run')
+    run_parser.add_argument('--dbsize',   dest="dbsize", type=int, nargs='?', default=10000)
+    run_parser.add_argument('--duration', dest="duration", type=int, nargs='?', default=60)
     run_parser.set_defaults(func=run)
     run_parser.set_defaults(callback=dummy)
 
